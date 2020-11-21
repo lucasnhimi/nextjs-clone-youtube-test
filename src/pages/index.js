76a8fd78d265a85f5dc48047dd02a1fcb1425ab8
Lucas <lucaslonguinhos@gmail.com>
2020-11-21 12:11:34 -0300

    Config Material-UI

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 export default function Home() {
   return (
     <div>
       <h1>Clone Youtube com Next.js + Material-UI</h1>
+      <Button variant="contained" color="primary">
+        Entrar
+      </Button>
     </div>
   );
 }
