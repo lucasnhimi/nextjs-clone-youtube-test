7b07ed03db139bbdf53dd5ad5cf72a57d2e265b0
Lucas <lucaslonguinhos@gmail.com>
2020-11-23 17:11:08 -0300

    Video card component

---
+++ src/components/VideoCard.js
@@ -0,0 +1,47 @@
+import { Box, Typography, Avatar, makeStyles } from '@material-ui/core';
+import moment from 'moment';
+
+const useStyles = makeStyles(() => ({
+  caption: {
+    fontWeight: 500,
+    display: '-webkit-box',
+    '-webkit-line-clamp': 2,
+    '-webkit-box-orient': 'vertical',
+    overflow: 'hidden',
+  },
+}));
+
+function VideoCard({ item }) {
+  const classes = useStyles();
+
+  return (
+    <Box>
+      <img style={{ width: '100%' }} alt={item.title} src={item.thumb} />
+      <Box display="flex" mt={1}>
+        <Box mr={1}>
+          <Avatar alt="Author" src={item.avatar}>
+            SS
+          </Avatar>
+        </Box>
+        <Box>
+          <Typography
+            className={classes.caption}
+            gutterBottom
+            variant="body1"
+            color="textPrimary"
+          >
+            {item.title}
+          </Typography>
+          <Typography display="block" variant="body2" color="textSecondary">
+            {item.channel}
+          </Typography>
+          <Typography variant="body2" color="textSecondary">
+            {`${item.views} • ${moment(item.date).locale('pt-br').fromNow()}`}
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
+export default VideoCard;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,44 @@
 import React from 'react';
+import { Box, Grid } from '@material-ui/core';
 import Layout from 'src/components/Layout';
+import VideoCard from 'src/components/VideoCard';
 
-export default function Home() {
-  return <Layout title="YouTube">teste</Layout>;
+function Home({ data }) {
+  return (
+    <Layout title="YouTube">
+      <Box p={2}>
+        <Grid container spacing={4}>
+          {data.map((item) => (
+            <Grid key={item.id} item lg={2} md={3} sm={6} xs={12}>
+              <VideoCard item={item} />
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    </Layout>
+  );
 }
+
+export async function getStaticProps() {
+  const data = [
+    {
+      id: 1,
+      title:
+        'FEED DO USUÁRIO | Criando uma Rede Social com React.js e .NET Core #29',
+      channel: 'Lucas Nhimi',
+      views: '11 mi de visualizações',
+      date: '01/11/2020',
+      avatar:
+        'https://yt3.ggpht.com/ytc/AAUvwnjsAqYw8F72hHnk-aZsOIJG3_Yf_AjF4cfekZ-A=s176-c-k-c0x00ffffff-no-rj',
+      thumb:
+        'https://i.ytimg.com/vi/eg9yLyb8mdM/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBjDZp2thAhoXe4VnvcH47CFQYUoQ',
+    },
+  ];
+  return {
+    props: {
+      data,
+    }, // will be passed to the page component as props
+  };
+}
+
+export default Home;
