382dcfbd71ce6c2e71c36cf56c40d97aa2ac9b75
Lucas <lucaslonguinhos@gmail.com>
2020-11-27 18:13:38 -0300

    Api endpoint insert videos

--- src/pages/api/video.js
+++ src/pages/api/video.js
@@ -1,4 +1,5 @@
-// import { connectToDatabase } from 'src/utils/mongodb';
+import { connectToDatabase } from 'src/utils/mongodb';
+import { ObjectId } from 'mongodb';
 import nextConnect from 'next-connect';
 import upload from 'src/utils/upload';
 
@@ -7,9 +8,23 @@ const handler = nextConnect();
 handler.use(upload.single('file'));
 
 handler.post(async (req, res) => {
-  console.log(req.file);
-  console.log(req.body);
-  return res.status(200).json({ imageUrl: req.file.location });
+  const { title, authorId, authorName, authorAvatar, videoUrl } = req.body;
+
+  const { db } = await connectToDatabase();
+  const collection = db.collection('videos');
+
+  await collection.insertOne({
+    title,
+    authorId: ObjectId(authorId),
+    authorName,
+    authorAvatar,
+    views: 0,
+    thumb: req.file.location,
+    videoUrl,
+    updatedAt: new Date(),
+  });
+
+  return res.status(200).json({ ok: true });
 });
 
 export const config = {
